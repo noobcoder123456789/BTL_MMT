457afe333e3ab8af3a61581ee54eb7b5319c9e84
noobcoder123456789 <152793384+noobcoder123456789@users.noreply.github.com>
2024-10-13 21:46:33 +0700

    Cập nhật full giao thức TCP
    Nhánh này chứa toàn bộ phần code Python hiện thực giao thức TCP + hỗ trợ multithreading trong việc yêu cầu file trong mạng P2P.

--- fileMake.py
+++ fileMake.py
@@ -1,15 +1,15 @@
 import os
 chunk_SIZE = 512 * 1024
 
-fileM = open("./Original_File/aCopy.pdf", "wb")
-
 SplitNum = 0
-dir_path = r'./Splitted_File'
+dir_path = './Local'
+# dir_path = './Splitted_File'
 for path in os.listdir(dir_path):
     SplitNum += os.path.isfile(os.path.join(dir_path, path)) is True
 
-for chunk in range(SplitNum):
-    fileT = open("./Splitted_File/chunk" + str(chunk) + ".txt", "rb")
+fileM = open("./Original_File/aCopy.pdf", "wb")
+for chunk in range(0, SplitNum):
+    fileT = open(dir_path + "/chunk" + str(chunk) + ".txt", "rb")
     byte = fileT.read(chunk_SIZE)
     fileM.write(byte)
 
