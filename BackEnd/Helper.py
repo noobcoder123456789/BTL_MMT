20a3e4a5bed2da4ea417a749c0862996685e7a36
QuocThinh73 <ttqthinh2004@gmail.com>
2024-11-05 10:32:35 +0700

    Chạy đa nền tảng

--- BackEnd/Helper.py
+++ BackEnd/Helper.py
@@ -9,9 +9,19 @@ chunk_SIZE = 512 * 1024
 tracker_url = "http://192.168.179.17:18000"
 
 
+# def get_wireless_ipv4():
+#     for interface, addrs in psutil.net_if_addrs().items():
+#         if "Wi-Fi" in interface or "Wireless" in interface or "wlan" in interface:
+#             for addr in addrs:
+#                 if addr.family == socket.AF_INET:
+#                     return addr.address
+#     return None
+
+
 def get_wireless_ipv4():
     for interface, addrs in psutil.net_if_addrs().items():
-        if "Wi-Fi" in interface or "Wireless" in interface or "wlan" in interface:
+        # Check for common wireless interface names across platforms
+        if any(wireless in interface.lower() for wireless in ["wi-fi", "wireless", "wlan", "en0", "airport"]):
             for addr in addrs:
                 if addr.family == socket.AF_INET:
                     return addr.address
@@ -31,6 +41,7 @@ def get_peers_count(tracker_url):
     if response.status_code == 200:
         peer_count = response.json().get('peer_count', 0)
         return peer_count
+
 
 def remove_chunk_list():
     if platform.system() == "Windows":
